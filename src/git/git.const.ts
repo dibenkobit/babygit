a650c5a43f28730376a5a876d2babb2425915bd6
Nikita Snetkov <lukyanish@gmail.com>
2025-02-27 21:18:42 +0500

    chore: add Git constants

---
+++ src/git/git.const.ts
@@ -0,0 +1,10 @@
+export const IGNORED_FILES = [
+    'bun.lock',
+    'bun.lockb',
+    'package-lock.json',
+    'yarn.lock',
+    'pnpm-lock.yaml',
+    'npm-shrinkwrap.json'
+];
+
+export const IGNORED_EXTENSIONS = ['.png', '.jpg', '.jpeg', '.gif', '.svg', '.ico', '.webp', '.bmp', '.tiff', '.avif'];
