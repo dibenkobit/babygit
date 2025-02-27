c9c75428268759508b6a6548b3d04108b5502945
Nikita Snetkov <lukyanish@gmail.com>
2025-02-27 21:18:42 +0500

    chore: add constants used across the application

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const AUTH_REDIRECT_URL = 'http://localhost:9918/auth/callback';
+export const WEB_AUTH_URL = 'https://babygit.dev/auth';
+export const API_URL = 'https://wdrmjfymllrxlkzmmlns.supabase.co/functions/v1';
